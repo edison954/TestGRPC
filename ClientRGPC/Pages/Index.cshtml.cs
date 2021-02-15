06d03059def07f2827970fe6bd4ef4204294bcf7
Edison Arley Plaza Marin <edison954@gmail.com>
2021-02-15 00:47:36 -0500

    grpc client and server

--- ClientRGPC/Pages/Index.cshtml.cs
+++ ClientRGPC/Pages/Index.cshtml.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Grpc.Net.Client;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using TestGRPC;
 
 namespace ClientRGPC.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly Greeter.GreeterClient cliente;
         private readonly ILogger<IndexModel> _logger;
         [BindProperty]
         public string Nombre { get; set; }
+        public string Mensaje { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
+            var url = "https://localhost:5001";
+            var canal = GrpcChannel.ForAddress(url);
+            cliente = new Greeter.GreeterClient(canal);
             _logger = logger;
         }
 
@@ -24,9 +31,14 @@ namespace ClientRGPC.Pages
 
         }
 
-        public void OnPost()
+        public async Task OnPost()
         {
+            var hellowRequest = new HelloRequest();
+            hellowRequest.Name = Nombre;
 
+            var resultado = await cliente.SayHelloAsync(hellowRequest);
+
+            Mensaje =   resultado.Message;
         }
 
     }
